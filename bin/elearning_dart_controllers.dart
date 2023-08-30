2da89a94a92b42c6d5facc6e4845bafcca40dafe
mofoka <thekoetlisi@gmail.com>
2023-08-30 13:29:52 +0200

    first commit

---
+++ bin/elearning_dart_controllers.dart
@@ -0,0 +1,5 @@
+import 'package:elearning_dart_controllers/elearning_dart_controllers.dart' as elearning_dart_controllers;
+
+void main(List<String> arguments) {
+  print('Hello world: ${elearning_dart_controllers.calculate()}!');
+}
---
+++ test/elearning_dart_controllers_test.dart
@@ -0,0 +1,8 @@
+import 'package:elearning_dart_controllers/elearning_dart_controllers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
